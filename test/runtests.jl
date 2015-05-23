1e324d3b30032a4f371d59f96054d1579e9e6167
Yuri D'Elia <yuri.delia@eurac.edu>
2015-05-24 01:06:04 +0200

    Add tests for make_bond().

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,27 @@ using Base.Test
 
 TIMEOUT = parse(Int, get(ENV, "BOND_TIMEOUT", "1"))
 
+# We assume all drivers are tested in the reference Python implementation.
+# As such, we only test one driver (Python) in order to cover the current host.
+
+# default cmd/args
 py = make_bond("Python"; timeout=TIMEOUT)
 ret = beval(py, "1")
 @assert ret == 1
 #close(py) # TODO: needs Expect 0.1.2
+
+# default args only
+py = make_bond("Python", `python`; timeout=TIMEOUT)
+ret = beval(py, "1")
+@assert ret == 1
+
+# break the command without arguments
+@test_throws make_bond("Python", `ssh localhost python`, timeout=TIMEOUT, def_args=false)
+
+# check default arguments with custom cmd
+py = make_bond("Python", `ssh localhost python`, timeout=TIMEOUT)
+ret = beval(py, "1")
+@assert ret == 1
+
+# broken command
+@test_throws make_bond("Python", `false`; timeout=TIMEOUT)
