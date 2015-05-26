c40b4dd8debb160d0f8ad71b67e8805ff1145973
Yuri D'Elia <yuri.delia@eurac.edu>
2015-05-26 16:41:09 +0200

    Fix Julia 0.3

--- test/test_marshalling_except.jl
+++ test/test_marshalling_except.jl
@@ -7,7 +7,11 @@ beval(py, """def test_id(arg):
     return arg
 """; stm=false)
 
-ex = @catch_except BondSerializationException bcall(py, "test_id", STDIN)
+# NOTE: JSON.json is currently too lax[1], allowing unknown/unhandled types to
+# slip though (functions/IO/etc). We test the serialization using a Module
+# type, which is explicitly handled as a failure.
+# [1] https://github.com/JuliaLang/JSON.jl/issues/108
+ex = @catch_except BondSerializationException bcall(py, "test_id", Base)
 @test ex.remote === false
 @test beval(py, "1") === 1
 
